8bb173ca0f258fda61ea9ee5ecc03eeea29d6c7b
David Cibin <ddcibin@gmail.com>
2024-10-03 15:41:09 -0400

    format tidegraph

--- src/TideGraph.tsx
+++ src/TideGraph.tsx
@@ -1,248 +1,221 @@
-import React, { useEffect, useState } from 'react';
-import LineChart from 'react-native-chart-kit/dist/line-chart';
+import React, { useEffect, useState } from "react";
+import LineChart from "react-native-chart-kit/dist/line-chart";
 import {
-  Dimensions,
-  View,
-  ActivityIndicator,
-  StyleSheet,
-  // Pressable,
-  Text,
-} from 'react-native';
-import axios from 'axios';
-import moment from 'moment';
-// import DateTimePicker, {
-//   DateTimePickerEvent,
-// } from '@react-native-community/datetimepicker';
+    Dimensions,
+    View,
+    ActivityIndicator,
+    StyleSheet,
+    Text,
+} from "react-native";
+import axios from "axios";
+import moment from "moment";
 
-import DropdownMenu from './utils/DropdownMenu';
+import DropdownMenu from "./utils/DropdownMenu";
 
-export default function TideGraph({ coordinates }: { coordinates: number[] }): React.JSX.Element {
-  /*****************************************************************/
-  /* State */
-  const [loading, setLoading] = useState(true);
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(moment().add(1, 'days').toDate());
-  const [showStartDatePicker, setShowStartDatePicker] = useState(false);
-  const [showEndDatePicker, setShowEndDatePicker] = useState(false);
-  const [allTideStations, setAllTideStations] = useState();
-  const [nearestTideStations, setNearestTideStations] = useState<any[]>([]);
-  const [stationName, setStationName] = useState<string>("");
+export default function TideGraph({
+    coordinates,
+}: {
+    coordinates: number[];
+}): React.JSX.Element {
+    /*****************************************************************/
+    /* State */
+    const [loading, setLoading] = useState(true);
+    const [nearestTideStations, setNearestTideStations] = useState<any[]>([]);
+    const [stationName, setStationName] = useState<string>("");
+    const [tideData, setTideData] = useState({
+        labels: [],
+        datasets: [{ data: [] }],
+    });
 
-  const [tideData, setTideData] = useState({
-    labels: [],
-    datasets: [{ data: [] }],
-  });
-
-  /*****************************************************************/
-    /* Data Fetching */
+    /*****************************************************************/
+    /* Helper Functions */
     // Function to calculate the distance between two points using the Haversine formula
-    function haversineDistance(lat1: number, lon1: number, lat2: number, lon2: number) {
-      const R = 6371; // Radius of the Earth in km
-      const dLat = (lat2 - lat1) * Math.PI / 180;
-      const dLon = (lon2 - lon1) * Math.PI / 180;
-      const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      const distance = R * c; // Distance in km
-      return distance;
+    function haversineDistance(
+        lat1: number,
+        lon1: number,
+        lat2: number,
+        lon2: number
+    ) {
+        const R = 6371; // Radius of the Earth in km
+        const dLat = ((lat2 - lat1) * Math.PI) / 180;
+        const dLon = ((lon2 - lon1) * Math.PI) / 180;
+        const a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos((lat1 * Math.PI) / 180) *
+                Math.cos((lat2 * Math.PI) / 180) *
+                Math.sin(dLon / 2) *
+                Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = R * c; // Distance in km
+        return distance;
     }
-  
-    // Example function to filter stations based on a given radius (in km)
-    function filterStationsByRadius(stations: any, lat: number, lon: number, radius: number) {
-      console.log("ALL STATIONS", stations.length, "LAT && LONG", lat, lon, "RADIUS", radius);
-      return stations.filter((station: any) => {
-        const distance = haversineDistance(lat, lon, parseFloat(station.lat), parseFloat(station.lng));
-        return distance <= radius;
-      });
-    }
-  
-    const getTide = async (lat: number, lon: number) => {
-      try {
-        const response = await axios.get(
-          `https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=tidepredictions&units=english`
-        );
-        // console.log("ALL STATIONS", response.data.stations.length, "LAT && LONG", lat, lon);
-        console.log("FIRST STATION", response.data.stations[0]);
-        const nearbyStations = filterStationsByRadius(response.data.stations, lat, lon, 50);
-        
-        console.log("NEARBY BY STATION ID", nearbyStations[0].id);
 
-        setNearestTideStations(nearbyStations);
-        setAllTideStations(response.data.stations);
-        fetchTideData(nearbyStations[0].id);
-      } catch (error: any) {
-        console.log(error);
-      }
+    // Example function to filter stations based on a given radius (in km)
+    function filterStationsByRadius(
+        stations: any,
+        lat: number,
+        lon: number,
+        radius: number
+    ) {
+        console.log(
+            "ALL STATIONS",
+            stations.length,
+            "LAT && LONG",
+            lat,
+            lon,
+            "RADIUS",
+            radius
+        );
+        return stations.filter((station: any) => {
+            const distance = haversineDistance(
+                lat,
+                lon,
+                parseFloat(station.lat),
+                parseFloat(station.lng)
+            );
+            return distance <= radius;
+        });
+    }
+
+    /*****************************************************************/
+    /* Data Fetching */
+    const getTide = async (lat: number, lon: number) => {
+        try {
+            const response = await axios.get(
+                `https://api.tidesandcurrents.noaa.gov/mdapi/prod/webapi/stations.json?type=tidepredictions&units=english`
+            );
+            const nearbyStations = filterStationsByRadius(
+                response.data.stations,
+                lat,
+                lon,
+                50
+            );
+            setNearestTideStations(nearbyStations);
+            fetchTideData(nearbyStations[0].id);
+        } catch (error: any) {
+            console.log(error);
+        }
     };
-    
 
     const fetchTideData = async (station: string) => {
-      try {
-        setLoading(true);
-        const response = await axios.get(
-          `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&datum=MLLW&begin_date=${moment(
-            startDate,
-          ).format('MM/DD/YYYY')}&range=30&interval=hilo&units=english&time_zone=lst_ldt&format=json&station=${station}`
-        );
-    
-        const predictions = response.data.predictions;
-        
-        const labels = predictions.map((prediction: any) =>
-          moment(prediction.t).format('hh:mmA')
-        );
-        const data = predictions.map((prediction: any) =>
-          parseFloat(prediction.v)
-        );
-        
-        setTideData({ labels, datasets: [{ data }] });
-      } catch (error: any) {
-        console.error('Error fetching tide data:', error.message);
-      } finally {
-        setLoading(false);
-      }
+        try {
+            setLoading(true);
+            const response = await axios.get(
+                `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&datum=MLLW&begin_date=${moment(
+                    new Date()
+                ).format(
+                    "MM/DD/YYYY"
+                )}&range=30&interval=hilo&units=english&time_zone=lst_ldt&format=json&station=${station}`
+            );
+
+            const predictions = response.data.predictions;
+            const labels = predictions.map((prediction: any) =>
+                moment(prediction.t).format("hh:mmA")
+            );
+            const data = predictions.map((prediction: any) =>
+                parseFloat(prediction.v)
+            );
+
+            setTideData({ labels, datasets: [{ data }] });
+        } catch (error: any) {
+            console.error("Error fetching tide data:", error.message);
+        } finally {
+            setLoading(false);
+        }
     };
-    
 
-  // useEffect(() => {
-  //   fetchTideData();
-  // }, [startDate, endDate]);
-  
-  useEffect(() => {
-    const [lat, lon] = coordinates;
-    getTide(lat, lon);
-  }, [coordinates]);
-  
-  // useEffect(() => {
-  //   // console.log("ERROR ???????", nearestTideStations);
-  //   if (!nearestTideStations.length) return;
-  //   const station = nearestTideStations[0].id;
-  //   fetchTideData(station);
-  // }, [coordinates, nearestTideStations]);
+    /*****************************************************************/
+    /* Effects */
+    useEffect(() => {
+        const [lat, lon] = coordinates;
+        getTide(lat, lon);
+    }, [coordinates]);
 
-  // const onStartDateChange = (
-  //   event: DateTimePickerEvent,
-  //   date?: Date | undefined,
-  // ) => {
-  //   const currentDate = date || startDate;
-  //   setShowStartDatePicker(false);
-  //   setStartDate(currentDate);
-  // };
+    /*****************************************************************/
+    /* Render */
+    return (
+        <View style={styles.container}>
+            {nearestTideStations && nearestTideStations.length > 0 && (
+                <DropdownMenu
+                    nearbyStations={nearestTideStations}
+                    fetchTideData={fetchTideData}
+                    setStationName={setStationName}
+                />
+            )}
 
-  // const onEndDateChange = (event: DateTimePickerEvent, date?: Date | undefined) => {
-  //   const currentDate = date || endDate;
-  //   setShowEndDatePicker(false);
-  //   setEndDate(currentDate);
-  // };
-
-  return (
-    <View style={styles.container}>
-      {nearestTideStations && nearestTideStations.length > 0 && (
-          <DropdownMenu nearbyStations={nearestTideStations} fetchTideData={fetchTideData} setStationName={setStationName} />
-      )}
-
-      {loading && tideData ? (
-        <View style={styles.loaderContainer}>
-          <ActivityIndicator size="large" color="#3a92da" />
+            {loading && tideData ? (
+                <View style={styles.loaderContainer}>
+                    <ActivityIndicator size="large" color="#3a92da" />
+                </View>
+            ) : (
+                <View style={styles.chartContainer}>
+                    <View style={styles.legendContainer}>
+                        <Text style={styles.legendText}>{stationName}</Text>
+                    </View>
+                    <LineChart
+                        data={tideData}
+                        width={Dimensions.get("window").width * 1.05}
+                        height={220}
+                        withHorizontalLines={false}
+                        withVerticalLines={false}
+                        fromZero={true}
+                        yAxisSuffix="ft"
+                        chartConfig={{
+                            backgroundColor: "#2a4c6d",
+                            backgroundGradientFrom: "#2a4c6d",
+                            backgroundGradientTo: "#2a4c6d",
+                            decimalPlaces: 1,
+                            color: (opacity = 1) =>
+                                `rgba(255, 255, 255, ${opacity})`,
+                            labelColor: (opacity = 1) =>
+                                `rgba(255, 255, 255, ${opacity})`,
+                            propsForDots: {
+                                r: "6",
+                                strokeWidth: "2",
+                                stroke: "#6b9bcc",
+                            },
+                        }}
+                        bezier
+                        style={styles.chart}
+                    />
+                </View>
+            )}
         </View>
-      ) : (
-        <View style={styles.chartContainer}>
-          <View style={styles.legendContainer}>
-            <Text style={styles.legendText}>{stationName}</Text>
-          </View>
-          <LineChart
-            data={tideData}
-            width={Dimensions.get('window').width * 1.05}
-            height={220}
-            withHorizontalLines={false}
-            withVerticalLines={false}
-            fromZero={true}
-            yAxisSuffix="ft"
-            chartConfig={{
-              backgroundColor: '#2a4c6d',
-              backgroundGradientFrom: '#2a4c6d',
-              backgroundGradientTo: '#2a4c6d',
-              decimalPlaces: 1,
-              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-              propsForDots: {
-                r: '6',
-                strokeWidth: '2',
-                stroke: '#6b9bcc',
-              },
-            }}
-            bezier
-            style={styles.chart}
-          />
-        </View>
-      )}
-    </View>
-  );
+    );
 }
 
+/*****************************************************************/
+/* Styles */
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
-  loaderContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  dateContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    margin: 10,
-  },
-  button: {
-    display: "none", // TODO: buttons are hidden for now
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: '#3a92da',
-    margin: 5,
-  },
-  btnText: {
-    fontSize: 14,
-    lineHeight: 18,
-    fontWeight: 'bold',
-    letterSpacing: 0.25,
-    color: 'white',
-  },
-  locationText: {
-    color: 'white',
-    fontSize: 15,
-    // fontWeight: 'bold',
-    textShadowColor: '#333',
-    textShadowOffset: { width: -1, height: 1 },
-    textShadowRadius: 5,
-    paddingRight: 5,
-  },
-  chartContainer: {
-    backgroundColor: '#2a4c6d',
-    borderRadius: 20,
-    padding: 10,
-    overflow: "hidden",
-    width: "95%",
-  },
-  chart: {
-    marginLeft: -5,
-    paddingTop: 12,
-  },
-  legendContainer: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center', // Center the legend text
-  },
-  legendText: {
-    fontSize: 16,
-    color: 'white',
-  },
+    container: {
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+    },
+    loaderContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    chartContainer: {
+        backgroundColor: "#2a4c6d",
+        borderRadius: 20,
+        padding: 10,
+        overflow: "hidden",
+        width: "95%",
+    },
+    chart: {
+        marginLeft: -5,
+        paddingTop: 12,
+    },
+    legendContainer: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    legendText: {
+        fontSize: 16,
+        color: "white",
+    },
 });
